247a64b7ce7d19fc2a6f20cb03be1f24a517aeba
Rochdyath <rochdiath.bachabi@epitech.eu>
2023-04-16 13:58:24 +0100

    feat: monthly sales

--- sales.py
+++ sales.py
@@ -6,4 +6,16 @@ import plotly.colors as colors
 pio.templates.default = "plotly_white"
 
 data = pd.read_csv("Superstore.csv", encoding='latin-1')
-print(data.head())
+
+data['Order Date'] = pd.to_datetime(data['Order Date'])
+data['Ship Date'] = pd.to_datetime(data['Ship Date']) 
+data['Order Month'] = data['Order Date'].dt.month
+data['Order Year'] = data['Order Date'].dt.year
+data['Order Day of Week'] = data['Order Date'].dt.dayofweek
+
+def monthy_sales():
+    df = data.groupby('Order Month').size().reset_index(name='sales')
+    fig = px.line(df, y='counts', x='Order Month', title='Monthly sales')
+    fig.show()
+
+monthy_sales()
