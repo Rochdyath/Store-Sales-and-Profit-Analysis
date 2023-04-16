cf1823e3e82c2f70fc05e5332dd04f21fcf2ab9a
Rochdyath <rochdiath.bachabi@epitech.eu>
2023-04-16 12:30:03 +0100

    feat: datasets

---
+++ sales.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+import plotly.colors as colors
+pio.templates.default = "plotly_white"
+
+data = pd.read_csv("Superstore.csv", encoding='latin-1')
+print(data.head())
